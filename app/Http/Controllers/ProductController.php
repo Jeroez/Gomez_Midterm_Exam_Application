a4475ef908c7ae528794efcb8c3f15a128e81231
Jeroez <jeremiahbnjabgmz@gmail.com>
2025-02-24 20:53:55 +0800

    2nd commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = [
+            ["name" => "Tensura", "category" => "Anime Series", "price" => "$20"],
+            ["name" => "Overflow", "category" => "Anime Series", "price" => "$18"],
+            ["name" => "Code Geass", "category" => "Anime Series", "price" => "$25"],
+            ["name" => "Naruto", "category" => "Anime Series", "price" => "$22"]
+        ];
+
+        return view('products', compact('products'));
+    }
+}
